4c9be4e25923344c77c3396357e356d16cfbb259
Jeranguz <Jeremyu4231@gmail.com>
2026-03-23 09:20:49 -0600

    Rename IAM role for GitHub Actions to standardize naming convention

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "github_actions_front_role" {
-  name = "github-actions-front-role"
+  name = "github-actions-front"
 
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",
