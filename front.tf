811fc477dabed94b46020770b6ff4a169d01d9f8
Jeranguz <Jeremyu4231@gmail.com>
2026-03-23 09:20:29 -0600

    Update CloudFront distribution configuration to enforce TLS 1.2 and add price class

--- front.tf
+++ front.tf
@@ -50,8 +50,9 @@ resource "aws_cloudfront_distribution" "frontend_distribution" {
   default_root_object = "index.html"
 
   viewer_certificate {
-    acm_certificate_arn = var.acm_certificate_arn
-    ssl_support_method  = "sni-only"
+    acm_certificate_arn      = var.acm_certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 
   restrictions {
@@ -61,17 +62,15 @@ resource "aws_cloudfront_distribution" "frontend_distribution" {
   }
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "s3-frontend"
-
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "s3-frontend"
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
   }
   enabled         = true
   is_ipv6_enabled = true
+  price_class     = "PriceClass_100"
   aliases         = [var.domain_name, "www.${var.domain_name}"]
 }
 
